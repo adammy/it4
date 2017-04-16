03e48395ccb0a06461d7702dd4ff9b34a4b9a231
adammy <adammacdonaldcorrao@gmail.com>
2017-04-16 10:49:24 -0400

    Utilizing waypoints for section changing

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,4 +2,12 @@
 
   'use strict';
 
+  $('.section').waypoint({
+    handler: function (direction) {
+      $('#backgrounds > div').fadeOut(200);
+      $(`#backgrounds #${this.element.id}-bg`).fadeIn(200);
+    },
+    offset: '25%'
+  });
+
 }(jQuery));
