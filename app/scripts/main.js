262f346f4f98f83a2688b147299d8eba41197801
adammy <adammacdonaldcorrao@gmail.com>
2017-04-17 07:43:57 -0400

    Work

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,10 +2,16 @@
 
   'use strict';
 
+  let waypointsInit = false;
+
   $('.section').waypoint({
     handler: function (direction) {
-      $('#backgrounds > div').fadeOut(200);
-      $(`#backgrounds #${this.element.id}-bg`).fadeIn(200);
+      if (waypointsInit) {
+        $('#backgrounds > div').fadeOut(200);
+        $(`#backgrounds #${this.element.id}-bg`).fadeIn(200);
+        //$(`#${this.element.id} .animate`).removeClass('animate').addClass('animated');
+      }
+      waypointsInit = true;
     },
     offset: '25%'
   });
