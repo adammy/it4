2e2fb7099b31aa0c0b620a0efb60e185d45c1aac
adammy <adammacdonaldcorrao@gmail.com>
2017-04-17 13:51:55 -0400

    Work

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -16,4 +16,38 @@
     offset: '25%'
   });
 
+
+
+
+
+  var ctx = $('#myChart');
+  var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['Isaiah Thomas', 'Other Person'],
+      datasets: [{
+        label: 'A Label',
+        data: [29, 15],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255,99,132,1)',
+          'rgba(54, 162, 235, 1)'
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      }
+    }
+  });
+
 }(jQuery));
