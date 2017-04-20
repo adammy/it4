3e0c736f73adc3c95293fcc7251d866b90b93fd7
Adam MacDonald <adam.macdonald-corrao@vistana.com>
2017-04-20 15:08:13 -0400

    Start of new chart format

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -46,131 +46,35 @@
 	Chart.defaults.global.tooltips.displayColors = false;
 
 	// chart.js bar/rectangle defaults
-	Chart.defaults.global.elements.rectangle.backgroundColor = 'rgba(0, 131, 72, 0.3)';
+	Chart.defaults.global.elements.rectangle.backgroundColor = 'rgba(85, 85, 85, 0.3)';
+	Chart.defaults.global.elements.rectangle.borderColor = '#555555';
 	Chart.defaults.global.elements.rectangle.borderWidth = 1;
-	Chart.defaults.global.elements.rectangle.borderColor = '#008348';
 
-	// returns data object needed for bar graphs
-	let barData = function (categoryLabel, itData, comparisonData) {
-		return {
-			labels: [players['thomas'].name.last, players['average-guard'].name.last],
-			datasets: [
-				{
-					label: categoryLabel,
-					data: [itData, comparisonData],
-					backgroundColor: [players['thomas'].colors.bg, players['average-guard'].colors.bg],
-					borderColor: [players['thomas'].colors.border, players['average-guard'].colors.border]
-				}
-			]
-		}
-	}
-
-	// returns options object for bar graphs
-	let barOptions = function (stepSize = 10, min = 0, max = 30) {
-		return {
+	// init chart
+	let chartElement = $('#chart');
+	let chart = new Chart(chartElement, {
+		type: 'bar',
+		data: {
+			labels: ['Thomas', 'Curry', 'Irving', 'Thomas', 'Curry', 'Irving', 'Thomas', 'Curry', 'Irving'],
+			datasets: [{
+				label: 'PTS',
+				data: [29.8, 25.4, 23.5, 29.8, 25.4, 23.5, 29.8, 25.4, 23.5],
+				backgroundColor: ['rgba(0, 131, 72, 0.3)', 'rgba(253, 185, 39, 0.3)', 'rgba(134, 0, 56, 0.3)', 'rgba(0, 131, 72, 0.3)', 'rgba(253, 185, 39, 0.3)', 'rgba(134, 0, 56, 0.3)', 'rgba(0, 131, 72, 0.3)', 'rgba(253, 185, 39, 0.3)', 'rgba(134, 0, 56, 0.3)'],
+				borderColor: ['#008348', '#fdb927', '#860038', '#008348', '#fdb927', '#860038', '#008348', '#fdb927', '#860038']
+			}]
+		},
+		options: {
 			scales: {
 				yAxes: [{
 					ticks: {
 						beginAtZero: true,
-						stepSize: stepSize,
-						min: min,
-						max: max
+						stepSize: 10,
+						min: 0,
+						max: 30
 					}
 				}]
 			}
 		}
-	}
-
-	// returns a string formatted as 6'1''; takes in inches arg
-	let heightString = function (inches) {
-		let feet = parseInt(inches / 12);
-		let inch = parseInt(inches % 12);
-		return `${feet}'${inch}''`;
-	}
-
-	// ppg chart
-	let ppgChart = new Chart($('#ppg-chart'), {
-		type: 'bar',
-		data: barData('PTS', players['thomas'].stats.pts, players['average-guard'].stats.pts),
-		options: barOptions()
 	});
-
-	// 4q ppg chart
-	let fourthppgChart = new Chart($('#4q-ppg-chart'), {
-		type: 'bar',
-		data: barData('4Q PTS', players['thomas'].stats.fqpts, players['average-guard'].stats.fqpts),
-		options: barOptions(5, 0, 10)
-	});
-
-	// true shooting % chart
-	let tsChart = new Chart($('#ts-chart'), {
-		type: 'bar',
-		data: barData('TS%', players['thomas'].stats.ts, players['average-guard'].stats.ts),
-		options: barOptions(0.33333, 0, 1)
-	});
-
-	// per chart
-	let perChart = new Chart($('#per-chart'), {
-		type: 'bar',
-		data: barData('PER', players['thomas'].stats.per, players['average-guard'].stats.per),
-		options: barOptions()
-	});
-
-	// on change of player field
-	$('select#compare').on('change', function () {
-		let player = $(this).val();
-		updateCharts(player);
-		$(this).children('.hide').remove();
-	});
-
-	// update charts with player argument
-	function updateCharts(player) {
-
-		let fname = players[player].name.first;
-		let lname = players[player].name.last;
-		let height = heightString(players[player].height);
-		let weight = players[player].weight;
-		let pts = players[player].stats.pts;
-		let fqpts = players[player].stats.fqpts;
-		let per = players[player].stats.per;
-		let ts = players[player].stats.ts;
-		let bgColor = players[player].colors.bg;
-		let borderColor = players[player].colors.border;
-
-		// update name, height, weight, and image data
-		$('#compare-name').text(`${fname} ${lname}`);
-		$('#compare-ht').text(height);
-		$('#compare-wt').text(weight);
-		$('#compare-img').attr('src', `//placehold.it/223x364?text=${lname}`);
-
-		// stat updates
-		ppgChart.data.datasets[0].data[1] = pts;
-		fourthppgChart.data.datasets[0].data[1] = fqpts;
-		perChart.data.datasets[0].data[1] = per;
-		tsChart.data.datasets[0].data[1] = ts;
-
-		// label updates
-		ppgChart.data.labels[1] = lname;
-		fourthppgChart.data.labels[1] = lname;
-		perChart.data.labels[1] = lname;
-		tsChart.data.labels[1] = lname;
-
-		// color updates
-		ppgChart.data.datasets[0].backgroundColor[1] = bgColor;
-		fourthppgChart.data.datasets[0].backgroundColor[1] = bgColor;
-		perChart.data.datasets[0].backgroundColor[1] = bgColor;
-		tsChart.data.datasets[0].backgroundColor[1] = bgColor;
-		ppgChart.data.datasets[0].borderColor[1] = borderColor;
-		fourthppgChart.data.datasets[0].borderColor[1] = borderColor;
-		perChart.data.datasets[0].borderColor[1] = borderColor;
-		tsChart.data.datasets[0].borderColor[1] = borderColor;
-
-		// deploy updated chart data
-		ppgChart.update();
-		fourthppgChart.update();
-		perChart.update();
-		tsChart.update();
-
-	}
 
 }(jQuery));
